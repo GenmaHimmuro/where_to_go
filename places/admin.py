c2c4bb08b9773c2453ecc7a3d316051563e32712
GenmaHimmuro <t.suleimanov2@gmail.com>
2025-07-27 18:29:23 +0500

    images have been added to the organizer model

--- places/admin.py
+++ places/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 from places.models import Organizers, Image
 
 
-admin.site.register(Organizers)
-admin.site.register(Image)
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
+@admin.register(Organizers)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title', 'id', ]
+    search_fields = ['title', ]
+    inlines = [
+        ImageInline,
+    ]
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['organizer',]
+    search_fields = ['organizer',]
